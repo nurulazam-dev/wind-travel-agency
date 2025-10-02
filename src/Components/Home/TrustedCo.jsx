86fd4a73b75b740564f61eeb754d926e8e9b8aeb
mnaofficialbd <mna.official92@gmail.com>
2025-10-02 23:36:19 +0600

    TrustedCo component update

--- src/Components/Home/TrustedCo.jsx
+++ src/Components/Home/TrustedCo.jsx
@@ -4,10 +4,8 @@ import airLogo from "../../Assets/images/TrustedCo/air-asia.png";
 
 const TrustedCo = () => {
   return (
-    <section className="section my-5 py-4">
-      <h1 className="text-3xl font-bold text-#14183E mx-12">
-        Trusted By Companies
-      </h1>
+    <section className="section p-12 pt-5 my-10 bg-slate-100 flex flex-col justify-center items-center gap-8">
+      <h1 className="text-3xl font-bold text-#14183E">Trusted By Companies</h1>
       <div className="flex w-full items-center justify-evenly">
         <div>
           <img src={philippineLogo} alt="" />
