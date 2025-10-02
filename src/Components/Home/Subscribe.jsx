d3d5bcdd61dd3cae0218be3d4da8de4da669a242
mnaofficialbd <mna.official92@gmail.com>
2025-10-02 23:14:30 +0600

    subscribe component update

--- src/Components/Home/Subscribe.jsx
+++ src/Components/Home/Subscribe.jsx
@@ -1,3 +1,4 @@
+import { FaEnvelope } from "react-icons/fa6";
 import subsImg from "../../Assets/images/Icons/subscribe.png";
 
 const Subscribe = () => {
@@ -7,17 +8,23 @@ const Subscribe = () => {
         style={{ background: `url(${subsImg})`, backgroundSize: "cover" }}
         className="mx-20 py-12"
       >
-        <h2 className="text-[33px] font-bold text-center">
+        <h2 className="text-[33px] font-bold text-center text-slate-600">
           Subscribe to get information, latest news and other
           <br /> interesting offers about WindTravel
         </h2>
+
         <div className="flex mt-10 justify-center">
-          <input
-            type="text"
-            placeholder="Your email"
-            class="input input-md w-full max-w-xs shadow-xl"
-          />
-          <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-md mx-3">
+          <div className="relative group">
+            <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none text-muted-foreground transition-colors group-focus-within:text-foreground">
+              <FaEnvelope className="text-slate-500" />
+            </div>
+            <input
+              type="text"
+              placeholder="Your email"
+              className="input input-md w-full shadow-xl rounded-md pl-10 pr-7 py-3"
+            />
+          </div>
+          <button className="bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-3 px-6 rounded-md ml-4 shadow-lg">
             Subscribe
           </button>
         </div>
