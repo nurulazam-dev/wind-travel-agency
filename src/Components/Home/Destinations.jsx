f66e0f056ff12650142a1389a6c128760c42a21e
mnaofficialbd <mna.official92@gmail.com>
2025-10-02 11:46:40 +0600

    destinations component update

--- src/Components/Home/Destinations.jsx
+++ src/Components/Home/Destinations.jsx
@@ -5,9 +5,9 @@ const Destinations = () => {
   const { title, subtitle } = sell;
 
   return (
-    <section className="section mt-10">
+    <section className="section my-5 px-12">
       <div className="container mx-auto">
-        <div className="mb-10">
+        <div className="mb-5">
           <p className="text-center text-[18px]">{subtitle}</p>
           <h2 className="text-center font-semibold text-[50px]">{title}</h2>
         </div>
--- src/Components/Home/DestinationsCards.jsx
+++ src/Components/Home/DestinationsCards.jsx
@@ -1,24 +1,28 @@
 import { sell } from "../../Assets/data/dataBank";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const DestinationsCards = () => {
   const { cards } = sell;
 
   return (
     <>
-      <div className="flex flex-col gap-y-[30px] lg:flex-row lg:gap-x-[15px] justify-center items-center">
+      <div className="flex flex-col  lg:flex-row mx-5 justify-center items-center">
         {cards.map((card) => {
           const { icon, title, price, subtitle } = card;
           return (
-            <div class="card card-compact w-96 bg-base-100 shadow-xl mx-5">
-              <div class="card-body">
-                <img src={icon} alt="Service" />
-                <div className="mb-3 text-[18px] flex text-center">
-                  <p>{title}</p>
-                  <p>${price}k</p>
-                </div>
-                <p className="mb-6 text-[16px] text-light text-center">
-                  {subtitle}
-                </p>
+            <div className="bg-slate-100 shadow-2xl mx-5 rounded-2xl text-slate-700 font-semibold">
+              <img
+                src={icon}
+                alt="Service"
+                className="w-full h-48 object-cover rounded-t-2xl"
+              />
+              <div className="mt-3 text-[16px] text-justify px-3 flex justify-between mb-2">
+                <p>{title}</p>
+                <p>${price}k</p>
+              </div>
+              <div className="flex items-center gap-2 px-3 mb-5 text-justify">
+                <FaLocationArrow className="text-xl mr-1" />
+                <p className="text-[15px]">{subtitle}</p>
               </div>
             </div>
           );
