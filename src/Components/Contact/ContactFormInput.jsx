f2c628c8e01d4bac7b0e4c79b6384620afbca776
mnaofficialbd <mna.official92@gmail.com>
2025-10-05 00:04:43 +0600

    update the ContactFormInput component

--- src/Components/Contact/ContactFormInput.jsx
+++ src/Components/Contact/ContactFormInput.jsx
@@ -1,7 +1,20 @@
-import React from "react";
-
-const ContactFormInput = () => {
-  return <div>Contact Form Input</div>;
-};
+const ContactFormInput = ({
+  type,
+  placeholder,
+  name,
+  value,
+  onChange,
+  required = true,
+}) => (
+  <input
+    type={type}
+    placeholder={placeholder}
+    name={name}
+    value={value}
+    onChange={onChange}
+    required={required}
+    className="w-full p-4 bg-gray-700 text-white placeholder-gray-400 border border-gray-700 rounded-lg focus:ring-2 focus:ring-yellow-500 focus:border-yellow-500 transition duration-200"
+  />
+);
 
 export default ContactFormInput;
