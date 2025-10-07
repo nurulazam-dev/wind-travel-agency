62dd8078d74e10ff1fd148be97ac72a2c82088ec
mnaofficialbd <mna.official92@gmail.com>
2025-10-07 11:22:53 +0600

    update the CategoryCard component

--- src/Components/Home/CategoryCards.jsx
+++ src/Components/Home/CategoryCards.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { service } from "../../Assets/data/dataBank";
 
-const Cards = () => {
+const CategoryCards = () => {
   const [index, setIndex] = useState(1);
   const { cards } = service;
 
   return (
-    <div className="flex flex-col lg:flex-row gap-6 justify-center items-center mx-5">
-      {cards.map((card, cardIndex) => {
+    <div className="flex flex-col lg:flex-row gap-6 justify-center items-center">
+      {cards?.map((card, cardIndex) => {
         const { icon, title, subtitle } = card;
         return (
           <div key={cardIndex}>
@@ -15,8 +15,8 @@ const Cards = () => {
               onClick={() => setIndex(cardIndex)}
               className={`${
                 index === cardIndex &&
-                "bg-slate-200 border  border-slate-300 shadow-2xl"
-              } p-3 rounded-lg bg-slate-50 text-center shadow-md cursor-pointer transition-all`}
+                "bg-slate-900 border  border-yellow-300 text-yellow-300 shadow-yellow-300"
+              } p-3 rounded-lg bg-slate-900 border  border-slate-300 text-center shadow-md cursor-pointer hover:shadow-yellow-300 hover:text-yellow-300 transition-all`}
             >
               <div className="mb-2">
                 <img src={icon} alt="" className="w-16 h-16 mx-auto" />
@@ -31,4 +31,4 @@ const Cards = () => {
   );
 };
 
-export default Cards;
+export default CategoryCards;
