4187346e281e6d3576c71e5f4e41f941ed37073f
mnaofficialbd <mna.official92@gmail.com>
2025-10-07 22:31:04 +0600

    update the subscribe component

--- src/Components/Home/Subscribe.jsx
+++ src/Components/Home/Subscribe.jsx
@@ -11,12 +11,12 @@ const Subscribe = () => {
       <div className="border rounded-full bg-yellow-600 text-white p-2 h-14 w-14 flex justify-center items-center absolute -right-[20px] top-[4%] translate-y-[-50%]">
         <BsFillSendFill className="w-8 h-7" />
       </div>
-      <h2 className="text-[33px] font-bold text-center text-slate-600">
-        Subscribe to get information, latest news and other
-        <br /> interesting offers about WindTravel
+      <h2 className="text-center font-bold lg:text-4xl md:text-3xl text-2xl text-slate-300 max-w-4xl mx-auto">
+        Subscribe to get information, latest news and other interesting offers
+        about WindTravel
       </h2>
 
-      <div className="flex mt-10 justify-center">
+      <div className="flex mt-8 justify-center">
         <div className="relative group">
           <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none text-muted-foreground transition-colors group-focus-within:text-foreground">
             <FaEnvelope className="text-slate-500" />
